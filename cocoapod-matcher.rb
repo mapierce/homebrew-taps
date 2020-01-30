794c4d07095154d3f44cb16ae36372c5740b1291
Matthew Pierce <mapierce@tcd.ie>
2020-01-30 19:01:58 +0000

    Updated taps

--- cocoapod-matcher.rb
+++ cocoapod-matcher.rb
@@ -1,10 +1,10 @@
 class CocoapodMatcher < Formula
   desc "A Homebrew package to write missing CocoaPod versions to the Podfile from the Podfile.lock"
   homepage ""
-  url "https://github.com/mapierce/CocoaPod-Matcher/archive/0.0.5.tar.gz"
-  sha256 "cf01d1dd71237402d509d05b7094729aaff63e21fd927d0da3335624204a7b62"
-  
+  url "https://github.com/mapierce/CocoaPod-Matcher/archive/0.0.6.tar.gz"
+  sha256 "82b217eb4ae6658d64ed2bb6c6639b5b44e89cd30870e6aa93211fa957997f16"
+
   def install
-    bin.install "pod-matcher"
+    bin.install "cocoapod-matcher"
   end
 end
