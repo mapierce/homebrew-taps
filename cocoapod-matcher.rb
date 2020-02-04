efe19948e26891d2d7ed15a1af04753bccf33b3d
Matthew Pierce <mpierce@currencyfair.com>
2020-02-04 16:04:58 +0000

    Updated homepage

--- cocoapod-matcher.rb
+++ cocoapod-matcher.rb
@@ -1,6 +1,6 @@
 class CocoapodMatcher < Formula
   desc "A Homebrew package to write missing CocoaPod versions to the Podfile from the Podfile.lock"
-  homepage ""
+  homepage "https://lookwhatmattmade.com/projects/cocoapod-matcher/"
   url "https://github.com/mapierce/CocoaPod-Matcher/archive/1.0.0.tar.gz"
   sha256 "522525d6d2b47b601aa54e06aa4c6971e82551acf42f82d52371f3984f3a1f95"
 
